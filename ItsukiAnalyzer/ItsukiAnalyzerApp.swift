7a60c7bae0db65120de43ce662643dd309b7078d
itsuki <itsuki.enjyoy@gmail.com>
2024-08-11 12:26:49 +0900

    Initial Commit

---
+++ ItsukiAnalyzer/ItsukiAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsukiAnalyzerApp.swift
+//  ItsukiAnalyzer
+//
+//  Created by Itsuki on 2024/08/11.
+//
+
+import SwiftUI
+
+@main
+struct ItsukiAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
